78091a6139616194e370b88077b043874f89b08d
alexey001234567 <64755311+alexey001234567@users.noreply.github.com>
2020-06-08 03:08:28 +0500

    Add files via upload

--- webWorker2.js
+++ webWorker2.js
@@ -1,20 +1,26 @@
-	// var i = 0;
+	var i = 0;
 	var nam = null;
 
-	// function timedCount() {
-	  // i = i + 1;
-	  // postMessage(i);
-	  // setTimeout("timedCount()",500);
-	// }
+	function timedCount() {
+	  i = i + 1;
+	  postMessage(i);
+	  setTimeout("timedCount()",500);
+	}
 
 	onmessage  = function (e){
 		if (e.data =='start'){
 			nam = 'John name';
 			postMessage(nam);
 		}
+		
+		if (e.data =='counter'){
+			 timedCount();
+		}
+		
 	
 	}
 	
+ 
 	
 	// self.addEventListener('message', function(e) {
   // postMessage(x * 2); // Намеренная ошибка. 'x' не определено.
