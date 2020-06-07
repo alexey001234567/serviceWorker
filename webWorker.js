2e576cb324c08971214f2bff0c59a46c93fd3a86
alexey001234567 <64755311+alexey001234567@users.noreply.github.com>
2020-06-08 02:59:08 +0500

    Add files via upload

--- webWorker.js
+++ webWorker.js
@@ -1,14 +1,23 @@
-	var i = 0;
+	// var i = 0;
 	var nam = null;
 
-	function timedCount() {
-	  i = i + 1;
-	  postMessage(i);
-	  setTimeout("timedCount()",500);
-	}
+	// function timedCount() {
+	  // i = i + 1;
+	  // postMessage(i);
+	  // setTimeout("timedCount()",500);
+	// }
 
-	function namesd(){
-		nam = 'John name';
-		postMessage(nam);
+	onmessage  = function namesd(e){
+		if (e.data =='start'){
+			nam = 'John name';
+			postMessage(nam);
+		}
 	
 	}
+	
+	
+	// self.addEventListener('message', function(e) {
+  // postMessage(x * 2); // Намеренная ошибка. 'x' не определено.
+// };
+	
+	
